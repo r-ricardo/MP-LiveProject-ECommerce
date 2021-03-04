dcbd9e3bf794024a29b69b53d7319e42c671f891
Ricardo Rubio <r_ricardo@outlook.com>
2021-03-03 21:03:08 -0600

    Initial commit

---
+++ src/main/java/com/ecommerce/mvc/controllers/RootController.java
@@ -0,0 +1,17 @@
+package com.ecommerce.mvc.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+	private final static Logger log = LoggerFactory.getLogger(RootController.class);
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
